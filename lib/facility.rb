fd8bbed225c64d8126c7e0ba6965ff5c178c66fa
Jackson Bickler <jbickler4@gmail.com>
2024-10-17 15:48:31 -0600

    initial commit

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,15 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(data)
+    @name = data[:name]
+    @address = data[:address]
+    @phone = data[:phone]
     @services = []
+    data = {name:, address:, phone:}
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
