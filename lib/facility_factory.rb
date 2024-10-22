451a67d5ab997bb4c4c235d0095a9a03afa28324
Jackson Bickler <jbickler4@gmail.com>
2024-10-22 10:41:55 -0600

    FacilityFactory spec

---
+++ lib/facility_factory.rb
@@ -0,0 +1,13 @@
+class FacilityFactory
+
+  def create_facilities(facilities)
+    facilities.map do |data|
+      Facility.new(
+        name: data[:dmv_office],
+        address: data[:address_li],
+        phone: data[:phone],
+        services: data[:services_p],
+      )
+    end
+  end
+end
